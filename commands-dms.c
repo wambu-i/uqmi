f7bc669b7e32d893f0c0b0165d1209b9fb106280
Wambui Karuga <wambui@karuga.org>
2021-03-16 10:32:54 +0300

    uqmi/uim: add ICCID callback.
    Uses read transparent for new modems.
    
    Signed-off-by: Wambui Karuga <wambui@karuga.org>

--- commands-dms.c
+++ commands-dms.c
@@ -303,6 +303,7 @@ cmd_dms_unblock_pin2_prepare(struct qmi_dev *qmi, struct qmi_request *req, struc
 	return QMI_CMD_REQUEST;
 }
 
+/*
 static void cmd_dms_get_iccid_cb(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg)
 {
 	struct qmi_dms_uim_get_iccid_response res;
@@ -319,13 +320,14 @@ cmd_dms_get_iccid_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct q
 	qmi_set_dms_uim_get_iccid_request(msg);
 	return QMI_CMD_REQUEST;
 }
+*/
 
 static void cmd_dms_get_imsi_cb(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg)
 {
 	struct qmi_dms_uim_get_imsi_response res;
 
 	qmi_parse_dms_uim_get_imsi_response(msg, &res);
-	printf("%d\n", res.data.imsi);
+	printf("Data is %s\n", res.data.imsi);
 	if (res.data.imsi)
 		blobmsg_add_string(&status, NULL, res.data.imsi);
 }
--- commands-uim.h
+++ commands-uim.h
@@ -21,10 +21,11 @@
 
 #define __uqmi_uim_commands												\
 	__uqmi_command(uim_verify_pin1, uim-verify-pin1, required, QMI_SERVICE_UIM), \
-	__uqmi_command(uim_verify_pin2, uim-verify-pin2, required, QMI_SERVICE_UIM) \
+	__uqmi_command(uim_verify_pin2, uim-verify-pin2, required, QMI_SERVICE_UIM), \
+	__uqmi_command(uim_get_iccid, uim-get-iccid, required, QMI_SERVICE_UIM) \
 
 
 #define uim_helptext \
 		"  --uim-verify-pin1 <pin>:          Verify PIN1 (new devices)\n" \
 		"  --uim-verify-pin2 <pin>:          Verify PIN2 (new devices)\n" \
-
+		"  --uim-get-iccid <file path>:		 Get ICCID from SIM using specified file path" \
