a2729dd35f859acd6efbf8138553ecbdbfe28db5
Wambui Karuga <wambui@karuga.org>
2021-03-17 16:57:03 +0300

    uqmi/uim: add ICCID request callback.
    Signed-off-by: Wambui Karuga <wambui@karuga.org>

--- commands-uim.c
+++ commands-uim.c
@@ -19,6 +19,42 @@
  * Boston, MA 02110-1301 USA.
  */
 
+#define cmd_uim_verify_pin1_cb no_cb
+static enum qmi_cmd_result
+cmd_uim_verify_pin1_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg, char *arg)
+{
+	struct qmi_uim_verify_pin_request data = {
+		QMI_INIT_SEQUENCE(session_information,
+			.session_type = QMI_UIM_SESSION_TYPE_CARD_SLOT_1,
+			""
+		),
+		QMI_INIT_SEQUENCE(info,
+			.pin_id = QMI_UIM_PIN_ID_PIN1,
+			.pin_value = arg
+		)
+	};
+	qmi_set_uim_verify_pin_request(msg, &data);
+	return QMI_CMD_REQUEST;
+}
+
+#define cmd_uim_verify_pin2_cb no_cb
+static enum qmi_cmd_result
+cmd_uim_verify_pin2_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg, char *arg)
+{
+	struct qmi_uim_verify_pin_request data = {
+		QMI_INIT_SEQUENCE(session_information,
+			.session_type = QMI_UIM_SESSION_TYPE_CARD_SLOT_1,
+			""
+		),
+		QMI_INIT_SEQUENCE(info,
+			.pin_id = QMI_UIM_PIN_ID_PIN2,
+			.pin_value = arg
+		)
+	};
+	qmi_set_uim_verify_pin_request(msg, &data);
+	return QMI_CMD_REQUEST;
+}
+
 static char **split_string(const char *string) {
     printf("Splitting strings!\n");
 	char **result = malloc (sizeof (char *) * 3);
@@ -74,50 +110,25 @@ get_sim_file_id_and_path_with_separator(const char *file_path_str, uint16_t *fil
 	return true;
 }
 
-#define cmd_uim_verify_pin1_cb no_cb
-static enum qmi_cmd_result
-cmd_uim_verify_pin1_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg, char *arg)
+static void
+cmd_uim_get_iccid_cb(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg)
 {
-	struct qmi_uim_verify_pin_request data = {
-		QMI_INIT_SEQUENCE(session_information,
-			.session_type = QMI_UIM_SESSION_TYPE_CARD_SLOT_1,
-			""
-		),
-		QMI_INIT_SEQUENCE(info,
-			.pin_id = QMI_UIM_PIN_ID_PIN1,
-			.pin_value = arg
-		)
-	};
-	qmi_set_uim_verify_pin_request(msg, &data);
-	return QMI_CMD_REQUEST;
-}
+	struct qmi_uim_read_transparent_response res;
+	qmi_parse_uim_read_transparent_response(msg, &res);
 
-#define cmd_uim_verify_pin2_cb no_cb
-static enum qmi_cmd_result
-cmd_uim_verify_pin2_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg, char *arg)
-{
-	struct qmi_uim_verify_pin_request data = {
-		QMI_INIT_SEQUENCE(session_information,
-			.session_type = QMI_UIM_SESSION_TYPE_CARD_SLOT_1,
-			""
-		),
-		QMI_INIT_SEQUENCE(info,
-			.pin_id = QMI_UIM_PIN_ID_PIN2,
-			.pin_value = arg
-		)
-	};
-	qmi_set_uim_verify_pin_request(msg, &data);
-	return QMI_CMD_REQUEST;
+	if (res.set.card_result) {
+		printf("SW1 %d\n", res.data.card_result.sw1);
+	}
+	else {
+		printf("Error getting information!\n");
+	}
 }
-
-#define cmd_uim_get_iccid_cb no_cb
 static enum qmi_cmd_result
 cmd_uim_get_iccid_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg, char *arg)
 {
 	uint16_t id = 0;
 	uint8_t *path = NULL;
 
-	printf("Arguments passed %s\n", arg);
 
 	if (!get_sim_file_id_and_path_with_separator(arg, &id, &path, ","))
 		return QMI_CMD_EXIT;
