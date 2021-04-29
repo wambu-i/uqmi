a6be97e46903e08df380ada416a6a15303917c94
Wambui Karuga <wambui@karuga.org>
2021-04-29 04:33:14 +0300

    uqmi/uim: add PIN verification.
    Signed-off-by: Wambui Karuga <wambui@karuga.org>

--- commands-uim.c
+++ commands-uim.c
@@ -337,9 +337,9 @@ cmd_uim_set_pin_protection_prepare(struct qmi_msg *msg, char *arg)
 
 	struct qmi_uim_set_pin_protection_request request = {
 		QMI_INIT_SEQUENCE(session_information,
-                        .session_type = QMI_UIM_SESSION_TYPE_CARD_SLOT_1,
-                        .application_identifier = "{}"
-                ),
+            .session_type = QMI_UIM_SESSION_TYPE_CARD_SLOT_1,
+            .application_identifier = "{}"
+        ),
 		QMI_INIT_SEQUENCE(info,
 			.pin_id = uim_req_data.pin_id,
 			.pin_value = uim_req_data.pin,
@@ -413,4 +413,48 @@ cmd_uim_set_new_pin_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct
 {
 	uim_req_data.new_pin = arg;
 	return QMI_CMD_DONE;
+}
+
+static enum qmi_cmd_result
+cmd_uim_get_pin_status_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg, char *arg)
+{
+	qmi_set_uim_get_pin_status_request(msg);
+	return QMI_CMD_REQUEST;
+}
+
+#define cmd_uim_verify_pin1_cb no_cb
+static enum qmi_cmd_result
+cmd_uim_verify_pin1_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg, char *arg)
+{
+	printf("PIN is given as %s\n", arg);
+	struct qmi_uim_verify_pin_request data = {
+		QMI_INIT_SEQUENCE(session_information,
+			.session_type = QMI_UIM_SESSION_TYPE_CARD_SLOT_1,
+			.application_identifier = "{}"
+		),
+		QMI_INIT_SEQUENCE(info,
+			.pin_id = QMI_UIM_PIN_ID_PIN,
+			.pin = arg
+		)
+	};
+	qmi_set_uim_verify_pin_request(msg, &data);
+	return QMI_CMD_REQUEST;
+}
+
+#define cmd_uim_verify_pin2_cb no_cb
+static enum qmi_cmd_result
+cmd_uim_verify_pin2_prepare(struct qmi_dev *qmi, struct qmi_request *req, struct qmi_msg *msg, char *arg)
+{
+	struct qmi_uim_verify_pin_request data = {
+		QMI_INIT_SEQUENCE(session_information,
+			.session_type = QMI_UIM_SESSION_TYPE_CARD_SLOT_1,
+			.application_identifier = "{}"
+		),
+		QMI_INIT_SEQUENCE(info,
+			.pin_id = QMI_UIM_PIN_ID_PIN2,
+			.pin = arg
+		)
+	};
+	qmi_set_uim_verify_pin_request(msg, &data);
+	return QMI_CMD_REQUEST;
 }
